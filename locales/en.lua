4ea12062a4503698daa68d3c901860d15eb0fa22
Niknock HD <87417955+Niknock@users.noreply.github.com>
2024-03-15 19:38:49 +0100

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -5,5 +5,7 @@ Locales['en'] = {
     ['closer']                  = 'You need to be closer to a trunk!',
     ['outofveh']                = 'You must be outside of the vehicle!',
     ['noveh']                   = 'There is no vehicle nearby',
+    ['noitem']					= 'You dont have any tape on you!',
+    ['noitemm']					= 'You dont have any tape remover on you!',
 
 }

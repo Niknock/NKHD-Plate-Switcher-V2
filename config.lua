8d15066f679c12e0def979a12a4e89bc485f9d66
Niknock HD <87417955+Niknock@users.noreply.github.com>
2024-03-15 13:50:49 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,3 +5,5 @@ Config.ESX = 'new' -- new or old
 Config.Locales = 'de' -- en or de or custom
 
 Config.Debug = false -- false or true
+
+Config.OxTarget = false -- true or false, Updated Version comming soon, I hope

72e940966b8d084f884cacec767af631a0a0d10b
Niknock HD <87417955+Niknock@users.noreply.github.com>
2024-03-16 19:49:09 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,9 +3,12 @@ Config = {}
 Config.ESX = 'new' -- new or old
 
 Config.OxTarget = true -- true or false
+Config.OxInventory = false -- true or false, might not work, not Testet!
 
 Config.RemoveTapeRemover = true -- true or false if true it removes the tape_remover from the inventory
 
 Config.Locales = 'en' -- en or de or custom
+
+Config.AdvancedParking = false -- true or false, might not work, not Testet!
 
 Config.Debug = true -- false or true

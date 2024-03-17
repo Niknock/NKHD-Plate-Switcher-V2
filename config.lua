751b4efb48f4b85eaef269376e10137b048698fd
Niknock HD <87417955+Niknock@users.noreply.github.com>
2024-03-17 10:49:53 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,6 +5,10 @@ Config.ESX = 'new' -- new or old
 Config.OxTarget = true -- true or false
 Config.OxInventory = false -- true or false, might not work, not Testet!
 
+Config.ProgressBar = 'progressBars' -- ESX, progressBars or custom
+
+Config.FreezePlayer = false -- true or false, false if you do not want the player freezed while the animation
+
 Config.RemoveTapeRemover = true -- true or false if true it removes the tape_remover from the inventory
 
 Config.Locales = 'en' -- en or de or custom
